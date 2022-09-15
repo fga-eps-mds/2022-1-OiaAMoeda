4c5e5eb832421b5237d1de6767987881c4c9eb0f
Yago Passos <73550220+yagompassos@users.noreply.github.com>
2022-09-15 11:47:30 -0300

    implementando o html anteriormente feito depois da reestruturação

--- OiaMoeda/UI/Components/grafico.js
+++ OiaMoeda/UI/Components/grafico.js
@@ -2,13 +2,13 @@
 function LoadCharts(symbol) {
     new TradingView.widget(
         {
-            "autosize": true,
+            "autosize": false,
             "symbol": "BINANCE:" + symbol,
             "interval": "D",
             "timezone": "Etc/UTC",
             "theme": "light",
             "style": "100",
-            "scale": "1",
+            "scale": "20",
             "locale": "in",
             "toolbar_bg": "#f1f3f6",
             "enable_publishing": false,
