1ba40255f4134d1048ce685afc516ae3c6c329ed
jpanacleto2 <jpanacleto5@gmail.com>
2022-09-15 01:36:37 -0300

    restruturacoa server 2

--- OiaMoeda/server/server.js
+++ OiaMoeda/server/server.js
@@ -1,30 +1,24 @@
 const express = require('express')
 const server = express()
+var axios = require("axios")
+const fs = require('fs');
 
-var axios = require('axios').default
+server.get('/noticias', (req, res) => {
 
-const fs = require('fs')
-
-server.post('/noticias', (req, res) => {
-    // create a JSON object
     const news = (async () => {
-        const data = await axios(`https://newsapi.org/v2/everything?q=+bitcoin&language=pt&sortBy=relevance&apiKey=188f514b04db4f2586a4700cb306a7b8`)
-        //console.log(data)
-        fs.writeFile('news.json', JSON.stringify(data, null, 2), err => {
+        const { data } = await axios(`https://newsapi.org/v2/everything?q=+shakira&language=pt&sortBy=relevance&apiKey=188f514b04db4f2586a4700cb306a7b8`)
+
+        fs.writeFile('/home/anacleto/Documentos/Projetos/2022-1-OiaAMoeda/OiaMoeda/server/cache/news.json', JSON.stringify(data, null, 2), err => {
             if (err) throw new Error('algo está errado')
             console.log('tudo certo')
         })
-        return data
-    })
-    console.log(news)
-    res.send(news)
+
+        res.send(JSON.stringify(data, null, 2))
+    })();
+
+
 })
 
-server.listen(3001, () => {
-    console.log('Servidor de pé em: https://localhost:30001')
-    console.log('ctrl+c para derrubar o server')
+server.listen(3003, () => {
+    console.log("servidor de pe em http://localhost:3003")
 })
-
-
-
-
