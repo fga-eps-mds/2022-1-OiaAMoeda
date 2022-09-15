7171bcf0716e8d5ab6be07952bd4870ec20e9223
pabloheika <pablo0359@gmail.com>
2022-09-15 10:29:17 -0300

    Atuliza solicitações fontend

--- OiaMoeda/UI/Components/inicia.js
+++ OiaMoeda/UI/Components/inicia.js
@@ -1,19 +1,7 @@
-let cont = 0;
+trocar_moeda('BitCoin','BTCUSDT');
 function trocar_moeda(moeda, sigla) {
     $("#moeda_selecionada").html(moeda);
     LoadCharts(sigla);
     carrega_noticias(moeda);
     cont++;
 };
-// function inicia(){
-//     trocar_moeda('BitCoin','BTCUSDT');
-// };
-// inicia();
-/*$(document).ready(function(){
-    if (cont == 0)
-	trocar_moeda('BitCoin','BTCUSDT');
- });*/
- /* jQuery(function($) {
-    trocar_moeda('BitCoin','BTCUSDT');
-	// Códigos jQuery a serem executados quando a página carregar usando o pseudônimo (alias) $ de forma a não conflitar com outras bibliotecas JavaScript.
-});*/
