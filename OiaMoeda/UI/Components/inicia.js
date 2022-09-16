b577e0466666e81d1239d98a5bf4aefd0100bec6
Yago Passos <73550220+yagompassos@users.noreply.github.com>
2022-09-15 21:42:23 -0300

    alteraçoes e correções

--- OiaMoeda/UI/Components/inicia.js
+++ OiaMoeda/UI/Components/inicia.js
@@ -3,4 +3,8 @@ function trocar_moeda(moeda, sigla) {
     $("#moeda_selecionada").html(moeda);
     LoadCharts(sigla);
     carrega_noticias(moeda);
+
+    // inserindo o nome da moeda no html da página
+    console.log(moeda);
+    
 };
